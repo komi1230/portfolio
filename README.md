25f185a33976de4d36dee8d60dd5faa56874419b
Yusuke Kominami <komi@Yusuke.local>
2020-02-10 23:46:03 +0900

    test for deployment

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This is my portfolio created with React.
 
 Check this URL:
-[https://komi1230.github.io](https://komi1230.github.io)
+[https://komi1230.github.io/portfolio](https://komi1230.github.io/portfolio)
---
+++ docs/precache-manifest.6e792b0c1267bba626cb7459e8ea46f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6bca25713d0028ae092d4ea472efff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b24f75dea8ca724a95e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f9e4b2218683e474fdd5",
+    "url": "/static/js/2.518863d2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.518863d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b24f75dea8ca724a95e",
+    "url": "/static/js/main.287f661f.chunk.js"
+  },
+  {
+    "revision": "23601ce2d7bd68e79313",
+    "url": "/static/js/runtime-main.65c87d75.js"
+  }
+]);
