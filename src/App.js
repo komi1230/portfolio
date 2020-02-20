ca3d6cd0bb5b61b06ee3bf6e656d277f50c9d88c
Yusuke Kominami <komi@Yusuke.local>
2020-02-21 00:17:42 +0900

    added Header and Top

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from './components/Header'
+import Top from './components/Top'
+
 
 function App() {
   return (
-    <>
-      hogehogehoge.
-    </>
+    <div>
+      <Header />
+      <Top />
+    </div>
   );
 }
 
---
+++ src/components/Top.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import background from './images/background.jpg'
+
+export default function Top() {
+    return <img src={background} alt="BackgroundImage" width='100%' height='5%' />;
+};
