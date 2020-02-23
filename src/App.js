9db92090d9e260cfdacc3d880de281eca65613bb
Yusuke Kominami <komi@Yusuke.local>
2020-02-23 14:35:57 +0900

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from './components/Header'
 import Top from './components/Top'
 
 
 function App() {
   return (
     <div>
-      <Header />
       <Top />
     </div>
   );
--- src/components/Top.js
+++ src/components/Top.js
@@ -1,6 +1,55 @@
-import React from 'react';
-import background from './images/background.jpg'
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import "../index.css"
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  AppBarColor: {
+    backgroundColor: '#606060',
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 0.05,
+  },
+}));
+
+
+function Header() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <AppBar position="static" classes={{colorPrimary: classes.AppBarColor}}>
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              Home
+            </Typography>
+            <Typography variant="h6" className={classes.title}>
+              About
+            </Typography>
+            <Typography variant="h6" className={classes.title}>
+              Biography
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+
 
 export default function Top() {
-    return <img src={background} alt="BackgroundImage" width='100%' height='5%' />;
-};
+    return (
+        <div id="topview">
+            <Header/>
+            hogehoge
+        </div>
+    )
+}
